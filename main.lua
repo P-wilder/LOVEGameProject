7c6f99cdff11cd0b7ffe0530ebaa8ccaf59913b1
Persephone Wilder <shaneawilder@gmail.com>
2025-01-02 16:45:52 -0600

    Figured out how to make objects and call their methods

--- main.lua
+++ main.lua
@@ -1,7 +1,10 @@
+require "player"
+local player = nil
+local x, y
 function love.load()
-	W, H = 800, 600
-	X, Y = W / 2, H / 2
-	Walk = 0.4
+	W, H = 1024, 768
+	x, y = W / 2, H / 2
+	Walk = 0.45
 	Run = 0.7
 	Second = 0
 	love.window.setMode(W, H, {
@@ -13,36 +16,45 @@ function love.load()
 end
 
 function love.draw()
-	love.graphics.print("Hello World!", X - 50, Y)
+	love.graphics.print("Hello World!", x - 50, y)
 	love.graphics.print("seconds: " .. Second, 0, 0)
 	love.graphics.print("HELP", 760, 0)
+	player = Player:new(x, y, Walk, Run)
 end
 
 function love.update(dt)
 	Second = Second + dt
-	if not love.keyboard.isDown('lshift') then
-		if love.keyboard.isDown('w') then
-			Y = Y - Walk
-		elseif love.keyboard.isDown('s') then
-			Y = Y + Walk
+	Player.movement(player)
+	--[[local walk = 0.45
+	local run = 0.7
+	local isW = love.keyboard.isDown('w')
+	local isS = love.keyboard.isDown('s')
+	local isA = love.keyboard.isDown('a')
+	local isD = love.keyboard.isDown('d')
+	local isShift = love.keyboard.isDown('lshift')
+	if not isShift then
+		if isW then
+			Y = Y - walk
+		elseif isS then
+			Y = Y + walk
 		end
 
-		if love.keyboard.isDown('a') then
-			X = X - Walk
-		elseif love.keyboard.isDown('d') then
-			X = X + Walk
+		if isA then
+			X = X - walk
+		elseif isD then
+			X = X + walk
 		end
-	elseif love.keyboard.isDown('lshift') then
-		if love.keyboard.isDown('w') then
-			Y = Y - Run
-		elseif love.keyboard.isDown('s') then
-			Y = Y + Run
+	elseif isShift then
+		if isW then
+			Y = Y - run
+		elseif isS then
+			Y = Y + run
 		end
 
-		if love.keyboard.isDown('a') then
-			X = X - Run
-		elseif love.keyboard.isDown('d') then
-			X = X + Run
+		if isA then
+			X = X - run
+		elseif isD then
+			X = X + run
 		end
-	end
+	end]]--
 end
---
+++ player.lua
@@ -0,0 +1,41 @@
+Player = {x=0, y=0, walk=0, run=0}
+
+function Player:new(x, y, walk, run)
+        t = {x=x, y=y, walk=walk, run=run}
+        setmetatable(t, self)
+        self.__index = self
+        return t
+end
+
+function Player:movement()
+	local isW = love.keyboard.isDown('w')
+	local isS = love.keyboard.isDown('s')
+	local isA = love.keyboard.isDown('a')
+	local isD = love.keyboard.isDown('d')
+	local isShift = love.keyboard.isDown('lshift')
+	if not isShift then
+		if isW then
+			self.y = self.y - self.walk
+		elseif isS then
+			self.y = self.y + self.walk
+		end
+
+		if isA then
+			self.x = self.x - self.walk
+		elseif isD then
+			self.x = self.x + self.walk
+		end
+	elseif isShift then
+		if isW then
+			self.y = self.y - self.run
+		elseif isS then
+			self.y = self.y + self.run
+		end
+
+		if isA then
+			self.x = self.x - self.run
+		elseif isD then
+			self.x = self.x + self.run
+		end
+	end
+end
